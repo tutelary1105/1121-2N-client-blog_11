ea9bc46b93c0eb350544d5f92e3864e61ded90e7
tutelary1105 <tutelary1105@gmail.com>
2024-01-03 21:04:51 +0800

    supabase

--- src/db/clientSupabase.js
+++ src/db/clientSupabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://izfkkkxjvqncdvyzzpkv.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6Zmtra3hqdnFuY2R2eXp6cGt2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5OTQ0MzgzMiwiZXhwIjoyMDE1MDE5ODMyfQ.0tClY1KRBkhViC0bg0EM8TKJn1Pb5t_FHfHOqDs-W0Q";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
